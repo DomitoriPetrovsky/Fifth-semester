73be7f79dc7a3f1566e15bf9b7488f563f0dc0c8
Dmitry Petrovsky <2021490.d@gmail.com>
2021-10-04 12:38:57 +0300

    create control unit for multiplai

---
+++ OEVMIS/Files/multiplier_controller.vhd
@@ -0,0 +1,81 @@
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.NUMERIC_std.all;
+
+entity multiplier_controller is 
+    generic( N : natural );
+    port (
+            REQUEST : in std_logic;
+            CLK : in std_logic;
+            cm_start : out std_logic; 
+            cm_process: out std_logic; 
+            cm_result : out std_logic
+        );
+end multiplier_controller;
+
+
+architecture rtl of multiplier_controller is 
+
+    signal rg_done : std_logic;
+    signal fl_request : std_logic;
+    signal counter : natural range 0 to N+3;
+    signal temp_counter : natural range 0 to N+3;
+
+begin
+
+    process(clk, REQUEST)
+    begin 
+        if (falling_edge(REQUEST)) then
+            fl_request <= '1';
+        end if;
+
+        if (rising_edge(clk)) then 
+            if counter = 1 then 
+
+                cm_start <= '1';
+                cm_process <= '0';
+                cm_result <= '0';
+
+            elsif ((counter > 1) and (counter < N)) then 
+
+                cm_start <= '0';
+                cm_process <= '1';
+
+            elsif counter = N then 
+
+                cm_process <= '0';
+                cm_result <= '1';
+                fl_request <= '0';
+
+            else 
+
+                cm_start <= '0';
+                cm_process <= '0';
+                cm_result <= '0';
+                
+
+            end if;
+
+        end if;
+    end process;
+
+    process(clk)
+    begin
+        if rising_edge(clk) then 
+            if (fl_request = '1') then 
+                if (counter = 9) then 
+                    counter <= natural(0);
+                else 
+                    counter <= temp_counter;
+                end if;
+            end if;
+        end if;
+    end process;
+
+    
+temp_counter <= counter + natural(1);
+
+
+
+end rtl;
--- OEVMIS/Files/tb_file.vhd
+++ OEVMIS/Files/tb_file.vhd
@@ -2,10 +2,7 @@
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
-
-
-entity tb_file is
-		
+entity tb_file is		
 end tb_file;
 
 architecture Behavioral of tb_file is
@@ -24,6 +21,19 @@ component multiplier_operaiting is
 		
 end component multiplier_operaiting;
 
+component multiplier_controller is 
+    generic( N : natural );
+    port (
+            REQUEST : in std_logic;
+            CLK : in std_logic;
+            cm_start : out std_logic; 
+            cm_process: out std_logic; 
+            cm_result : out std_logic
+        );
+end component multiplier_controller;
+
+
+
 constant N : natural := 8;
 signal mn : std_logic_vector(N-1 downto 0);
 signal	mt : std_logic_vector(N-1 downto 0);
@@ -32,7 +42,7 @@ signal	cm_process : std_logic;
 signal	cm_result : std_logic;
 signal	clk : std_logic;
 signal	do : std_logic_vector(N*2-1 downto 0);
-
+signal request : std_logic;
 begin
 	
 cl: process
@@ -44,29 +54,35 @@ cl: process
 	
 	end process;
 	
-	process
+pr:	process
 	begin 
-	cm_start <= '0';
-	cm_process <= '0';
-	cm_result <= '0';
-	wait for 10 ns;
-	
-	cm_start <='1' ;
-	wait for 20 ns;
-	
-	cm_start <= '0';
-	cm_process <= '1';
-	wait for 70 ns;
-	
-
-	cm_process <= '0';
-	cm_result <= '1';
-	wait for 10 ns;
-	
-	cm_result <= '0';
-	
+	request <= '0';
+	wait for 21 ns;
+	request <= '1';
+	wait for 18 ns;
+	request <= '0'; 
 	wait;
-
+--	cm_start <= '0';
+--	cm_process <= '0';
+--	cm_result <= '0';
+--	wait for 10 ns;
+--	
+--	cm_start <='1' ;
+---	wait for 20 ns;
+--	
+--	cm_start <= '0';
+--	cm_process <= '1';
+--	wait for 70 ns;
+--	
+--
+--	cm_process <= '0';
+--	cm_result <= '1';
+--	wait for 10 ns;
+--	
+--	cm_result <= '0';
+--	
+--	wait;
+--
 	end process;
 	
 	mn <= "00000010";
@@ -82,6 +98,14 @@ dut_1	: multiplier_operaiting
 					cm_result => cm_result,
 					clk => clk,
 					do => do);
+
+dut_2	: multiplier_controller
+		generic map (N)
+		port map(REQUEST => request, 
+				CLK => clk, 
+				cm_start => cm_start, 
+				cm_process => cm_process, 
+				cm_result => cm_result);
 					
 			
 					
